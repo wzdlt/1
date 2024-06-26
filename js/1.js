91bbd66c72659b2aff1afc26a2a576a1bd411aa7
未知的路途 <1774954352@qq.com>
2024-06-26 14:58:06 +0800

    1

--- js/1.js
+++ js/1.js
@@ -1,11 +1,7 @@
 $(document).ready(function() {
     $('.title div').click(function() {
-        var clickedClass = $(this).attr('class'); // 获取点击元素的类名
-        
-        // 首先隐藏.text中所有的子div
+        var clickedClass = $(this).attr('class');
         $('.text > div').hide();
-        
-        // 显示与点击元素类名相同的.text中的div
         $('.text > .' + clickedClass).show();
     });
 });
